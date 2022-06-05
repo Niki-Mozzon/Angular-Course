c0d4956e1bd6d6550ab1d4219f7ea3ba385c33b4
mozzon <niki.mozzon@gmail.com>
2022-06-05 11:52:47 +0200

    56. Displaying Recipe Details

--- Recipe-app/src/app/recipe-boox/recipe-list/recipe-list.component.ts
+++ Recipe-app/src/app/recipe-boox/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
@@ -6,5 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Mammt',
+      'Mammt pasta',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
+    ),
+    new Recipe(
+      'Soup for poor',
+      'Così è la vita',
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'
+    ),
+  ];
+
   ngOnInit(): void {}
 }
