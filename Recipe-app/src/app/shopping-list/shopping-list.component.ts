6a8492fd91ed8a183d47718db1ef6e0ec6f37c01
mozzon <niki.mozzon@gmail.com>
2022-06-05 12:28:00 +0200

    60. Adding a Shopping List Edit Section

--- Recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../share/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
@@ -6,5 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Flour', 2),
+    new Ingredient('Poison', 5),
+  ];
+  constructor() {}
   ngOnInit(): void {}
 }
