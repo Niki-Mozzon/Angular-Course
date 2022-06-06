65b3848aad63c1bcb9b13033709b831ca357014b
EURIS\mozzon <niki.mozzon@euris.it>
2022-06-06 19:48:42 +0200

    ww

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type:"blueprint",name:"Test server",content:"Just a test!"}];
+
+  
+  onAddServer(serverData:{serverName:string,serverContent:string}) {
+ this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    }); 
+  }
+
+  onAddBlueprint(blueprintData:{serverName:string,serverContent:string}) {
+ this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    }); 
+  }
 }
--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
   selector: "cockpit",
@@ -6,6 +6,10 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["cockpit.component.css"],
 })
 export class CockpitComponent implements OnInit {
+  @Output()
+  serverCreated=new EventEmitter<{serverName:string,serverContent:string}>();
+  @Output()
+  blueprintCreated=new EventEmitter <{serverName:string,serverContent:string}>();
   newServerName = "";
   newServerContent = "";
 
@@ -13,6 +17,7 @@ export class CockpitComponent implements OnInit {
   constructor() {}
 
   onAddServer() {
+    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
     /* this.serverElements.push({
       type: "server",
       name: this.newServerName,
@@ -21,6 +26,7 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddBlueprint() {
+    this.blueprintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent})
     /* this.serverElements.push({
       type: "blueprint",
       name: this.newServerName,
