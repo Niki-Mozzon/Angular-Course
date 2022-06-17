1d1e742e50887593016a0ae22b151f3d2771fbf7
mozzon <niki.mozzon@gmail.com>
2022-06-18 01:05:46 +0200

    133. Passing Parameters to Routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from "@angular/forms";
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
+  { path: "user/:id", component: UserComponent },
   { path: "servers", component: ServersComponent },
 ];
 
