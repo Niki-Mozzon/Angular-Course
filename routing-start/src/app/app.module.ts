21f70d28426833a7f1318fa9494d9c27afa212e9
mozzon <niki.mozzon@gmail.com>
2022-06-18 17:00:56 +0200

    140. Setting up Child (Nested) Routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { FormsModule } from "@angular/forms";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "user/:id/:name", component: UserComponent },
-  { path: "users", component: UsersComponent },
-  { path: "servers/:id/edit", component: EditServerComponent },
-  { path: "servers/:id", component: ServerComponent },
-  { path: "servers", component: ServersComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
+  },
+  {
+    path: "servers",
+    component: ServersComponent,
+    children: [
+      { path: ":id", component: ServerComponent },
+      { path: ":id/edit", component: EditServerComponent },
+    ],
+  },
 ];
 
 import { AppComponent } from "./app.component";
