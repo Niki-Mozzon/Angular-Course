a7ca56c47b3a63704ceb7d2d1f45370adc1895f5
mozzon <niki.mozzon@gmail.com>
2022-06-23 22:10:18 +0200

    174. Building a Custom Observable

--- 13-Observables/src/app/user/user.component.ts
+++ 13-Observables/src/app/user/user.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: 'user.component.html',
   styleUrls: ['user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id!: number;
+  subscription?: Subscription;
+  customObs?: any;
   constructor(private route: ActivatedRoute) {}
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
   ngOnInit(): void {
     this.route.params.subscribe((param: Params) => {
       this.id = +param['id'];
     });
+
+    if (this.id === 1) {
+      this.customObs = new Observable((observer) => {
+        let count: number = 0;
+        setInterval(() => {
+          count++;
+          observer.next(count);
+        }, 1000);
+      });
+
+      this.subscription = this.customObs.subscribe((count: number) => {
+        console.log('Custom obs fires: ' + count);
+      });
+    }
   }
 }
