c648520750bfaea9e87db8d02225cc4b173e9ef9
mozzon <niki.mozzon@gmail.com>
2022-06-23 22:55:40 +0200

    177. Understanding Operators

--- 13-Observables/src/app/user/user.component.ts
+++ 13-Observables/src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { filter, map, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -27,30 +27,40 @@ export class UserComponent implements OnInit, OnDestroy {
         let count: number = 0;
         setInterval(() => {
           count++;
-          if (count > 5) {
-            observer.error('The count was bigger than 5!'); // I thow an error if bigger than 5, an error stops the observer but it doesn not complete it
+          if (count > 10) {
+            observer.error('The count was bigger than 5!'); // I throw an error if bigger than n, an error stops the observer but it doesn not complete it
           }
-          if (count > 3) {
+          if (count > 7) {
             observer.complete(); // I tell when to complete the obeserver, the oberserver will unsubscribe automatically, it doesn't take args
           }
           observer.next(count); //trigger the observer
         }, 1000);
       });
 
-      this.subscription = this.customObs.subscribe(
-        (count: number) => {
-          //Do this when it gets triggered
-          console.log('Custom obs fires: ' + count);
-        },
-        (error: string) => {
-          //Do this when there is an error
-          alert(error);
-        },
-        () => {
-          //Do this once completed
-          console.log('Complete!');
-        }
-      );
+      this.subscription = this.customObs
+        .pipe(
+          // a pipeline is a function that stays between the observer and the subscription, it influences the data just for the subscription
+          filter((data: number) => {
+            return data > 3; //filters all the data bigger than n
+          }),
+          map((data: number) => {
+            return 'Modified on pipe with map method => ' + data;
+          })
+        )
+        .subscribe(
+          (count: number) => {
+            //Do this when it gets triggered
+            console.log('Custom obs fires: ' + count);
+          },
+          (error: string) => {
+            //Do this when there is an error
+            alert(error);
+          },
+          () => {
+            //Do this once completed
+            console.log('Complete!');
+          }
+        );
     }
   }
 }
