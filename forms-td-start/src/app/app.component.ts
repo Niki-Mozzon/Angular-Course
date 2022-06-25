ae3df52fdea7dd7bda33adcc360b35e6864c9478
mozzon <niki.mozzon@gmail.com>
2022-06-25 17:10:44 +0200

    199. TD: Setting and Patching Form Values

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -13,9 +13,22 @@ export class AppComponent {
   //}
   answerQuestion: string = 'Placeholder';
   genders: string[] = ['Male', 'Female'];
+  userName?: string;
 
+  /* //One way to set all the fields at once, if there where some fields already edited the changes will be lost!
+   suggestUserName() {
+     this.form?.setValue({
+       userData: { username: 'superuser', email: 'awd@awd.ta' },
+       secret: 'pet',
+       questionAnswer: 'ciao',
+       gender: 'Male',
+      });
+    } 
+    */
+
+  //overrites just specific fields
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    this.form?.form.patchValue({ userData: { username: 'superciaone' } });
   }
 
   //2 ways to get the same result
